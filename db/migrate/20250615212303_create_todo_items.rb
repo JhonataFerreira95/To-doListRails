dd75985aa7766b8459ea6dd94d049ec854297dde
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-06-16 17:53:14 -0300

    adc ao schema na tabela todo-list e adc um valor padrão false, já que por padrão ele vem nulo

--- db/migrate/20250615212303_create_todo_items.rb
+++ db/migrate/20250615212303_create_todo_items.rb
@@ -3,7 +3,7 @@ class CreateTodoItems < ActiveRecord::Migration[7.1]
     create_table :todo_items do |t|
       t.belongs_to :todo_list, null: false, foreign_key: true
       t.string :text
-      t.boolean :completed, default: false
+      t.boolean :completed, default: false # adc um valor padrão false, já que por padrão ele vem nulo
 
       t.timestamps
     end
