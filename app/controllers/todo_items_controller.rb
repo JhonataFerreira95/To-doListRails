58af7a9f563ba92b4b3beee3f09d0e6ffa877ee9
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-06-22 23:16:55 -0300

    arrumado o html.erb do todo_items para funcinar com as rotas aninhadas...

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,33 +1,33 @@
 class TodoItemsController < ApplicationController
-  before_action:set_todo_list # Aqui criei uma ação que antes de você visualizar ou criar o item, você deve passar pela lista.
-  before_action :set_todo_item, only: %i[ show edit update destroy ]
+  before_action :set_todo_list
+  before_action :set_todo_item, only: %i[show edit update destroy]
 
-  # GET /todo_items or /todo_items.json
+  # GET /todo_lists/:todo_list_id/todo_items
   def index
-    @todo_items = @todo_list.todo_items # Aqui vou redenrizar um item da tarefa a partir da lista de tarefas
+    @todo_items = @todo_list.todo_items
   end
 
-  # GET /todo_items/1 or /todo_items/1.json
+  # GET /todo_lists/:todo_list_id/todo_items/:id
   def show
   end
 
-  # GET /todo_items/new
+  # GET /todo_lists/:todo_list_id/todo_items/new
   def new
-    @todo_item =  @todo_list.todo_items.new
+    @todo_item = @todo_list.todo_items.new
   end
 
-  # GET /todo_items/1/edit
+  # GET /todo_lists/:todo_list_id/todo_items/:id/edit
   def edit
   end
 
-  # POST /todo_items or /todo_items.json
+  # POST /todo_lists/:todo_list_id/todo_items
   def create
-    @todo_item =  @todo_list.todo_items.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: "item criado com sucesso" }
-        format.json { render :show, status: :created, location: @todo_item }
+        format.html { redirect_to todo_list_todo_item_path(@todo_list, @todo_item), notice: "Item criado com sucesso" }
+        format.json { render :show, status: :created, location: todo_list_todo_item_path(@todo_list, @todo_item) }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @todo_item.errors, status: :unprocessable_entity }
@@ -35,12 +35,12 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_items/1 or /todo_items/1.json
+  # PATCH/PUT /todo_lists/:todo_list_id/todo_items/:id
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: "item atualizado com sucesso" }
-        format.json { render :show, status: :ok, location: @todo_item }
+        format.html { redirect_to todo_list_todo_item_path(@todo_list, @todo_item), notice: "Item atualizado com sucesso" }
+        format.json { render :show, status: :ok, location: todo_list_todo_item_path(@todo_list, @todo_item) }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @todo_item.errors, status: :unprocessable_entity }
@@ -48,26 +48,26 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # DELETE /todo_items/1 or /todo_items/1.json
+  # DELETE /todo_lists/:todo_list_id/todo_items/:id
   def destroy
     @todo_item.destroy!
 
     respond_to do |format|
-      format.html { redirect_to todo_list_todo_items_path(@todo_list), status: :see_other, notice: "item deletado com sucesso" }
+      format.html { redirect_to todo_list_todo_items_path(@todo_list), status: :see_other, notice: "Item deletado com sucesso" }
       format.json { head :no_content }
     end
   end
 
   private
+
     def set_todo_list
-      @todo_list = TodoList.find(params[:todo_list_id]) # adc o todo_list para puxa as listas por id uma dentro da outra
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo_item
-      @todo_item =  @todo_list.todo_items.find(params[:id])
+      @todo_list = TodoList.find(params[:todo_list_id])
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
     def todo_item_params
       params.require(:todo_item).permit(:todo_list_id, :text, :completed)
     end
