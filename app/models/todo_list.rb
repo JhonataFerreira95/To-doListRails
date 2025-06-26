426200a90a302266b6f6591e4e709e146a970547
JhonataFerreira95 <jhonataovirowa@gmail.com>
2025-06-26 03:28:13 -0300

    atualizado html.erb do login para algo mais agradável aos olhos

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ApplicationRecord
+    belongs_to :user
     has_many:todo_items, dependent: :destroy # adc um has many para puxar os dados da tabela todo_items e um destroy para que a tarefa seja removida independete dos itens dentro dela
     alias_attribute :items, :todo_items # adc um alias para fica mais simples a chamada da classe
 end
